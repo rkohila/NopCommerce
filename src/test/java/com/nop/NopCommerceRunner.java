03e20c8e04003dcb62d548f08e15330f100325ba
rkohila <rkohila@gmail.com>
2017-06-08 22:54:30 +0100

    added cukes runner report format

--- src/test/java/com/nop/NopCommerceRunner.java
+++ src/test/java/com/nop/NopCommerceRunner.java
@@ -8,7 +8,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(glue = "com.nop.stepDefn",
         features = "src/test/resources/feature",
-        tags = "@validation")
+        tags = "@validation",
+           format = { "pretty", "html:target/cucumber"})
 public class NopCommerceRunner {
 
     // nothing to commit
